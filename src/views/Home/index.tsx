dc1303e48a8d0920e9f49bfc28389586a631f62e
Guilherme Gules Moreira <guilhermegules@gmail.com>
2022-03-07 22:05:12 -0300

    feat: creating home view

---
+++ src/views/Home/index.tsx
@@ -0,0 +1,8 @@
+import React, { FC } from 'react';
+import Clients from '../../components/Clients';
+
+import Header from '../../components/Header';
+
+const Home: FC = () => <Clients header={Header} />;
+
+export default Home;
